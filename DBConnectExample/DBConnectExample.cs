4a144acf4f0badbedbbb37c6bd23dc65dd6fb288
Barbara Engle <barbara@tshimologong.joburg>
2021-05-12 11:42:45 +0200

    Add project files.

---
+++ DBConnectExample/DBConnectExample.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DBConnectExample
+{
+    class DBConnectExample
+    {
+        static void Main(string[] args)
+        {
+
+            //Connection
+            //-------------------------------------------------------------------------------------------------
+
+            SqlConnection cn = new SqlConnection();
+
+            cn.ConnectionString = @"Server=(localdb)\MSSQLLocalDB;Database=AppFactory;Integrated Security=True;";
+            
+
+            //Command
+            //-------------------------------------------------------------------------------------------------
+            SqlCommand cmd = new SqlCommand();
+            
+            cmd.Connection = cn;
+            
+            cmd.CommandType = CommandType.Text;
+            
+            cmd.CommandText = "SELECT year FROM AppFactory WHERE ID = 1";
+
+            cn.Open();
+
+            var result = cmd.ExecuteScalar();
+            
+            Console.WriteLine(result);
+
+            cn.Close();
+
+            cn.Dispose();
+        }
+    }
+}
